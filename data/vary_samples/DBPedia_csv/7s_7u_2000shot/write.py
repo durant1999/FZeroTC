e9e2c74d8d8d1f467aa083538cae687692fe72a4
lvcunchi <lvcunchi@163.com>
2024-07-14 11:07:40 +0800

    add data files

---
+++ data/vary_samples/DBPedia_csv/7s_7u_2000shot/write.py
@@ -0,0 +1,25 @@
+import os
+
+
+
+stat_dict={}
+
+write_res=[]
+
+with open("./test_new.csv","r") as  f:
+    for line in f.readlines():
+        arr = line.split(',', 1)
+        class_label = int(eval(arr[0]))
+        if class_label not in stat_dict:
+            stat_dict[class_label] = 0
+        if stat_dict[class_label]>=2000: 
+            continue
+        else :
+            stat_dict[class_label] += 1
+            write_res.append(line)
+
+with open("./test_mine.csv","w") as f:
+    for line in write_res:
+        f.write(line)
+        
+ 
